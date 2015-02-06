27bff4d0acfb46a7840d2667ebb27f8570d3d5d7
Forrest Collman <forrest.collman@gmail.com>
2015-02-06 09:57:54 -0800

    turned integer and floating properties into spinboxes of appropriate types... string properties not currently functioning as i'm not sure how to properly validate them.. some properties described as not read-only by micromanager do appear to be read only in fact.

--- MMPropertyBrowser.py
+++ MMPropertyBrowser.py
@@ -35,9 +35,9 @@ class MMPropertyBrowser(QtGui.QWidget):
         self.layout = QtGui.QVBoxLayout() 
         self.mmc = mmc
         self.devices=self.mmc.getLoadedDevices() 
-        devicepulldown = QtGui.QComboBox(self)
-        devicepulldown.addItems(self.devices)
-        devicepulldown.currentIndexChanged.connect(self.deviceChanged)
+        self.devicepulldown = QtGui.QComboBox(self)
+        self.devicepulldown.addItems(self.devices)
+        self.devicepulldown.currentIndexChanged.connect(self.deviceChanged)
 
         self.propTable = QtGui.QTableWidget(self)
         device0 = self.devices[0]
@@ -45,7 +45,7 @@ class MMPropertyBrowser(QtGui.QWidget):
         #button = QtGui.QPushButton('Open Dialog', self)
 
         #button.clicked.connect(self.handleOpenDialog)
-        self.layout.addWidget(devicepulldown)
+        self.layout.addWidget(self.devicepulldown)
         self.layout.addWidget(self.propTable)
 
         self.setLayout(self.layout)
@@ -60,6 +60,7 @@ class MMPropertyBrowser(QtGui.QWidget):
         self.propTable.setHorizontalHeaderLabels(["property","value"])
         for i,prop in enumerate(props):
             propNameItem = QtGui.QTableWidgetItem(prop)
+            propNameItem.setFlags(QtCore.Qt.ItemIsEnabled)
             self.propTable.setItem(i,0,propNameItem)                      
             value = self.mmc.getProperty(device,prop)
             readOnly = self.mmc.isPropertyReadOnly(device,prop)
@@ -67,26 +68,82 @@ class MMPropertyBrowser(QtGui.QWidget):
             if len(allowedValues)>0  and not readOnly:
                 propValueItem = QtGui.QComboBox(self)
                 propValueItem.addItems(allowedValues)
+                #self.connect(propValueItem, QtCore.SIGNAL('currentIndexChanged(QString)'), self.setProperty,device)
+                propValueItem.currentIndexChanged[str].connect(functools.partial (self.setProperty,device,prop))
                 self.propTable.setCellWidget(i,1,propValueItem)
-            else:    
-                propValueText = QtGui.QTableWidgetItem(value)
-                self.propTable.setItem(i,1,propValueText)
-            if readOnly:
+            elif not readOnly:  
+                devType=self.mmc.getPropertyType(device,prop)
+                if devType == 1:
+                    #then this is a string
+                    print "need to fix"
+                elif devType == 2:
+                    #then this is a float
+                    propFloatSpin = QtGui.QDoubleSpinBox(self)
+                    propFloatSpin.setDecimals(3)
+                    if self.mmc.hasPropertyLimits(device,prop):
+                        maxval=self.mmc.getPropertyUpperLimit(device,prop)
+                        minval=self.mmc.getPropertyLowerLimit(device,prop)
+                        
+                    else:
+                        maxval=9999999.99
+                        minval=-9999999.99
+
+                    propFloatSpin.setMaximum(maxval)
+                    propFloatSpin.setMinimum(minval)
+                    print (device,prop,"min-max",minval,maxval)    
+
+                    self.propTable.setCellWidget(i,1,propFloatSpin)
+                    propFloatSpin.setKeyboardTracking(False)
+                    propFloatSpin.valueChanged[str].connect(functools.partial(self.setProperty,device,prop))
+                elif devType == 3:
+                    #then this is an integer
+                    propIntSpin = QtGui.QSpinBox(self)
+                    if self.mmc.hasPropertyLimits(device,prop):
+                        maxval=self.mmc.getPropertyUpperLimit(device,prop)
+                        minval=self.mmc.getPropertyLowerLimit(device,prop)
+                    else:
+                        minval=-9999999
+                        maxval=9999999
+
+                    propIntSpin.setMaximum(int(maxval))
+                    propIntSpin.setMinimum(int(minval))
+                    print (device,prop,"min-max",int(minval),int(maxval))
+                    self.propTable.setCellWidget(i,1,propIntSpin)
+                    propIntSpin.setKeyboardTracking(False)
+                    propIntSpin.valueChanged[str].connect(functools.partial(self.setProperty,device,prop))
+
+                
+            else:
+           
+
+
+                propValueItem = QtGui.QTableWidgetItem(value)
+                self.propTable.setItem(i,1,propValueItem)
+                propValueItem.setBackground(QtCore.Qt.lightGray)
+                propValueItem.setFlags(QtCore.Qt.ItemIsEnabled)
                 nameItem = self.propTable.item(i,0)
-               
                 nameItem.setBackground(QtCore.Qt.lightGray)
-                valueItem = self.propTable.item(i,1)
-                valueItem.setBackground(QtCore.Qt.lightGray)
-                valueItem.setFlags(QtCore.Qt.ItemIsEnabled)
+                
         i=len(props)
         propNameItem = QtGui.QTableWidgetItem("Device Type")
+        propNameItem.setFlags(QtCore.Qt.ItemIsEnabled)
+        propNameItem.setBackground(QtCore.Qt.lightGray)
         self.propTable.setItem(i,0,propNameItem)   
+
         device_type = self.mmc.getDeviceType(device)
-        print device_type
         propValueText = QtGui.QTableWidgetItem(self.mmc.getDeviceName(device))
+        propValueText.setFlags(QtCore.Qt.ItemIsEnabled)
+        propValueText.setBackground(QtCore.Qt.lightGray)
         self.propTable.setItem(i,1,propValueText)
 
+        #self.propTable.itemChanged.connect(self.cellChanged)
+    
 
+
+    def setProperty(self,device,prop,value):  
+        print device,prop,value 
+        self.mmc.setProperty(device,prop,str(value))
+        
     def handleOpenDialog(self):
         if self._dialog is None:
             self._dialog = QtGui.QDialog(self)
