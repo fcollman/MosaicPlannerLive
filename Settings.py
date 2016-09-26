8512a8ccf7397125eb0770ff1f1a52fa3b94a55c
Forrest Collman <forrest.collman@gmail.com>
2016-09-26 10:04:11 -0700

    added MultiRibbonSettings

--- Settings.py
+++ Settings.py
@@ -577,7 +577,7 @@ class MultiRibbonSettings(wx.Dialog): #MultiRibbons
         gridSizer.Add(wx.StaticText(self,id=wx.ID_ANY,label="choose"),border=5)
         #gridSizer.Add(wx.StaticText(self,id=wx.ID_ANY,label="0"),border=5)
         self.ribbon_label=wx.StaticText(self,id=wx.ID_ANY,label="0")
-        #self.ribbon_load_button=wx.Button(self,id=wx.ID_ANY,label="Load",name="load button")
+        self.ribbon_load_button=wx.Button(self,id=wx.ID_ANY,label="Load",name="load button")
         #self.ribbon_filepicker=wx.FilePickerCtrl(self,message='Select an array file',\
         #path="",name='arrayFilePickerCtrl1',\
         #style=wx.FLP_USE_TEXTCTRL, size=wx.Size(300,20),wildcard='*.*')
@@ -587,6 +587,7 @@ class MultiRibbonSettings(wx.Dialog): #MultiRibbons
         #self.ribbon_filepickersizer.Add(self.ribbon_filepicker,1,wx.EXPAND)
         #self.ribbon_filepickersizer.Add(self.ribbon_load_button,0,wx.EXPAND)
         gridSizer.Add(self.ribbon_label,0,wx.EXPAND,border=5)
+        gridSizer.Add(self.ribbon_load_button,0,wx.EXPAND,border=5)
 
         hbox = wx.BoxSizer(wx.HORIZONTAL)
         ok_button = wx.Button(self,wx.ID_OK,'OK')
